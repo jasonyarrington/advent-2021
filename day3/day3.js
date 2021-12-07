9e5aa011ab05ded144e807403119e53d1806963c
Jason Yarrington <jason@fastforward.sh>
2021-12-07 17:44:16 -0500

    Day 4 complete

--- day3/day3.js
+++ day3/day3.js
@@ -20,7 +20,12 @@ fs.readFile(path, 'utf8', (err, data) => {
 
     let { gamma, epsilon } = fuelConsumption(data)
     console.log("g", gamma, epsilon)
-    lifeSupportRating(data, gamma, epsilon)
+    console.log(data)
+    let o2reading = lifeSupportRating(data, 0, 'o2')
+    let co2reading = lifeSupportRating(data, 0, 'co2')
+
+    console.log('Readings', o2reading, co2reading, bitCalc(o2reading), bitCalc(co2reading), bitCalc(o2reading) * bitCalc(co2reading))
+
 })
 
 const fuelConsumption = (data) => {
@@ -57,32 +62,62 @@ const fuelConsumption = (data) => {
     }
 }
 
-const lifeSupportRating = (data) => {
+// O2reading - most often returned value
+// COSreading -- least often returned reading
 
-    let lifeSupport = 0;
-    console.log('Life support rating: ', lifeSupport);
-}
+const lifeSupportRating = (data, bit = 0, readingType) => {
 
-const commonBit = (data, index) => {
+    let bits = data[0].length
 
-    let dataLength = data.length;
-    let bitsLength = data[0].length;
+    // iterate over bits
 
-    // Find most common value in the bit index, and reduce to that
-    // Foreach bit
-    for (let bitIndex = 0; bitIndex < bitsLength; bitIndex++) {
-        let gammaCount = data.reduce((gamma, b, index) => {
-            let reading = Number(b[bitIndex]);
-            // console.log(gamma, b, index, bitIndex, b[bitIndex]);
-            return gamma + reading;
-        }, 0)
-        console.log(gammaCount);
+    let { gamma, epsilon } = bitCount(data, bit)
 
-        // If gamma count greater than half of total count
-        gamma += gammaCount / dataLength > .5 ? String(1) : String(0)
-        epsilon += gammaCount / dataLength < .5 ? String(1) : String(0)
+    let filter
+    // default is 0 if they are equal
+    switch (readingType) {
+        // most common
+        // default 1
+        case 'o2':
+            filter = epsilon > gamma ? '0' : '1'
+        break;
+        // least common
+        // default 0
+        case 'co2':
+            filter = gamma < epsilon ? '1' : '0'
+        break;
     }
 
+    data = data.filter(record => record[bit] == filter)
+
+    console.log('Life support rating: ', data);
+
+    if (bit < bits && data.length > 1) {
+        data = lifeSupportRating(data, bit + 1, readingType);
+    }
+
+    return typeof data == 'object' ? data[0] : data;
+}
+
+/**
+ * 
+ * @param {*} data  // array of data
+ * @param {*} index  // most frequent bit
+ * @returns 
+ * 
+ * returns distribution of bit count
+ */
+const bitCount = (data, bit) => {
+
+    // Gamma = 1
+    // Epsilon = 0
+
+    // For each record, get the bit count
+    let gamma = data.reduce((gamma, record) => {
+        return gamma + Number(record[bit])
+    }, 0)
+    let epsilon = data.length - gamma
+    console.log('gg', gamma, epsilon, data.length)
     return { gamma, epsilon }
 
 }
